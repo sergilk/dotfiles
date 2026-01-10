115fb091d4aa39f0413d073b4975563d4f25c045
szerjii <liashko.sergi@gmail.com>
2026-01-10 22:11:17 +0200

    feat(wal): obsidian support

--- .config/wal/post-run.sh
+++ .config/wal/post-run.sh
@@ -2,5 +2,6 @@
 
 # This is just a wrapper for running post-run scripts
 
-$HOME/.config/wal/scripts/dunst.sh
-$HOME/.config/wal/scripts/gtk.py -f $HOME/.cache/wal/gtk.css -t $HOME/.config
+"$HOME/.config/wal/scripts/dunst.sh"
+"$HOME/.config/wal/scripts/gtk.py" -f "$HOME/.cache/wal/gtk.css" -t "$HOME/.config"
+"$HOME/.config/wal/scripts/obsidian.sh" "$HOME/vault/obsidian/obsidian-vaults/main/.obsidian/snippets/obsidian.css"
---
+++ .config/wal/scripts/obsidian.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ln -sf "$XDG_CACHE_HOME/wal/obsidian.css" "$1" 
